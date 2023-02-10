86d9ab5acb8dc873745e0c0e990fcb3afc5eb502
CodingDude <sharpdt06@gmail.com>
2023-02-09 22:27:57 -0500

    added automatic ending of parenthesis and curly

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from tkinter import *
 
 
+def end_paren(*args):
+
+    insert = textbox.index(INSERT)
+    textbox.insert(insert, ")")
+
+
+def end_curly(*args):
+
+    insert = textbox.index(INSERT)
+    textbox.insert(insert, "}")
+
+
 
 HEIGHT = 400
 WIDTH = 50
@@ -8,9 +20,11 @@ WIDTH = 50
 
 window = Tk()
 
-textbox = Text(window, fg="white", bg="#2e2e2d", width=WIDTH, insertbackground="white")
-textbox.pack(fill="both", expand=True)
+window.bind("<(>", end_paren)
+window.bind("<{>", end_curly)
 
+textbox = Text(window, fg="white", bg="#2e2e2d", width=WIDTH, insertbackground="white", tabs=16)
+textbox.pack(fill="both", expand=True)
 
 
 commandbox = Text(window, bg="#2e2e2d", fg="white", width=WIDTH, height=1, insertbackground="white")
